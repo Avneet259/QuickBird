9863c0cdc31ea081bcf52f66d5e4a77512827b20
Avneet Kaur <avneetbal1325@gmail.com>
2024-11-26 22:13:10 +0530

    Add notification page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from './pages/auth/signup/SignUpPage'
 import LoginPage from './pages/auth/login/LoginPage'
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
 			</Routes>
 			<RightPanel />
 		</div>
